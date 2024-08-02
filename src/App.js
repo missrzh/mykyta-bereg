c142f0b310eebf18f4eb393f68c3d862f6f14228
missrzh <mishan162@gmail.com>
2024-08-02 04:33:51 +0300

    added themes and theme buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import FirstPageContent from './components/firstPageContent';
 
 function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
+
+  const setDarkTheme = () => {
+    setIsDarkTheme(true);
+  };
+
+  const setLightTheme = () => {
+    setIsDarkTheme(false);
+  };
+
   return (
-    <div className="App">
+    <div className={`App ${isDarkTheme ? 'dark-theme' : 'light-theme'}`}>
+      <div className='theme-button-container'>
+        <div className='theme-button theme-button-dark' onClick={setDarkTheme}></div>
+        <div className='theme-button theme-button-light' onClick={setLightTheme}></div>
+      </div>
+
       <FirstPageContent />
     </div>
   );
