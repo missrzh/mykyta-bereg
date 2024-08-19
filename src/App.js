56c8686b28d27cfbb8e3a6e15b7cb5be036b3093
missrzh <mishan162@gmail.com>
2024-08-19 17:12:35 +0300

    upadates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import FirstPageContent from './components/FirstPageContent';
-import VideoWorkPage from './components/VideoworkPage';
+import FirstPageContent from './components/firstPageContent';
+import VideoWorkPage from './components/videoworkPage';
 import { useSelector } from 'react-redux';
+
 
 function App() {
   const { hoveredVideo } = useSelector((state) => state.video);
@@ -14,14 +15,41 @@ function App() {
     }
   };
 
+
+
   return (
 
     <div className={`App dark-theme`}>
       {hoveredVideo && (
-        <video className="background-video" autoPlay loop muted playsInline preload="metadata" key={hoveredVideo.video}>
+        <video
+
+          autoPlay
+          loop
+          muted
+          playsInline
+          preload="metadata"
+          key={hoveredVideo.video}
+          style={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            width: '100%',
+            height: '100vh',
+            objectFit: 'cover',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 1,
+            pointerEvents: 'none',
+            backgroundImage: `url('${hoveredVideo.bg}')`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+
+          }}
+        >
           <source src={hoveredVideo.video} type="video/webm" />
           Your browser does not support the video tag.
         </video>
+
       )}
 
       <FirstPageContent onClick={scrollToVideoWork} />
